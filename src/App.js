49a1bbcc47e2d4630519259f3071a63bc4b8cff8
alfeifan <alanfeifan.li@gmail.com>
2021-02-16 17:44:52 +1100

    removed console.log for api call

--- src/App.js
+++ src/App.js
@@ -93,7 +93,6 @@ class App extends Component {
   }
 
   calculateFaceLocation = (data) => {
-    console.log(data);
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
     const image = document.getElementById('inputimage');
     const width = Number(image.width);
