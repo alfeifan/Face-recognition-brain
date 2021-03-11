bc1f021947cfe84d4188cdcb11b54ece6be67eff
alfeifan <alanfeifan.li@gmail.com>
2021-03-12 10:47:58 +1100

    corrected spelling

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -8,7 +8,7 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
 				{'This Magic Brain will detect faces in your pictures. Give it a try.'}
 			</p>
 			<p className='f3'>
-				{'Paste your image link into the text field below. Make sure that the image file ends in .jpg or .png'}
+				{'Paste your image link into the text field below. Make sure that the image link ends in .jpg or .png'}
 			</p>
 			<div className='center'>
 				<div className='form center pa4 br3 shadow-5'>
